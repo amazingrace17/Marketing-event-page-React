19ef261007f5d22f3f2af57b81057747ad036bcd
Tolulope Arinola <78857611+amazingrace17@users.noreply.github.com>
2022-02-21 16:19:05 +0100

    Commit from TeeportHQ

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './style.module.css'
+import Jlogin from './views/jlogin'
 import Home from './views/home'
 import Register from './views/register'
 import Login from './views/login'
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <Router>
       <div>
+        <Route exact component={Jlogin} path="/jlogin" />
         <Route exact component={Home} path="/" />
         <Route exact component={Register} path="/register" />
         <Route exact component={Login} path="/login" />
---
+++ src/views/jlogin.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import projectStyles from '../style.module.css'
+import styles from './jlogin.module.css'
+
+const Jlogin = () => {
+  return (
+    <div className={styles['container']}>
+      <Helmet>
+        <title>jlogin - Marketing event page</title>
+        <meta property="og:title" content="jlogin - Marketing event page" />
+      </Helmet>
+      <div className={styles['container1']}>
+        <img
+          alt="image"
+          src="/playground_assets/green-logo-1500w.png"
+          className={styles['image']}
+        />
+        <div className={styles['container2']}>
+          <form className={styles['Form']}>
+            <h1 className={styles['text']}>
+              <span>Sign in</span>
+            </h1>
+            <input
+              type="email"
+              name="email"
+              required="true"
+              placeholder="Email"
+              className={` ${styles['textinput']} ${projectStyles['input']} `}
+            />
+            <input
+              type="password"
+              name="password"
+              required="true"
+              placeholder="Password"
+              className={` ${styles['textinput1']} ${projectStyles['input']} `}
+            />
+            <span className={styles['text2']}>Forget password</span>
+            <button
+              type="submit"
+              className={` ${styles['button']} ${projectStyles['button']} `}
+            >
+              sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Jlogin
